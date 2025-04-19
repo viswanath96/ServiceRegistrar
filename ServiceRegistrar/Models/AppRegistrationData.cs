e3bd3a200356c76b25f6bd914e678f49b12510e0
links <links96.4u@outlook.com>
2025-04-19 13:55:45 +0530

    Add project files.

---
+++ ServiceRegistrar/Models/AppRegistrationData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceRegistrar.Models
+{
+    public class AppRegistrationData
+    {
+        public string ApplicationName { get; set; }
+        public string ExecutablePath { get; set; }
+        public string Description { get; set; }
+    }
+
+}
---
+++ ServiceRegistrar/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.Extensions.Configuration;
+using ServiceRegistrar.Models;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        IConfiguration configuration = new ConfigurationBuilder()
+            .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+            .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+            .Build();
+
+        List<AppRegistrationData> listOfAppRegistrationData = configuration.GetSection("AppRegistrationData").Get<List<AppRegistrationData>>();
+
+
+    }
+
+}
