8da8f67a2283134071c4f29f84c160f3e3cd1ad0
viswanath <links96.4u@outlook.com>
2025-04-20 17:35:30 +0530

    Added Trigger and Repetition classes.

--- TaskSchedulerLibrary/Models/AppRegistrationData.cs
+++ TaskSchedulerLibrary/Models/AppRegistrationData.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TaskSchedulerLibrary.Models;
 
 namespace ServiceRegistrar.Models
 {
@@ -13,4 +14,17 @@ namespace ServiceRegistrar.Models
         public string Description { get; set; }
     }
 
+    /// <summary>
+    /// Represents a Daily trigger for a scheduled task.
+    /// </summary>
+    public class Trigger
+    {
+        TimeBoundry StartTimeBoundry { get; set; }
+    }
+
+    public class Repetiton
+    {
+        TimeBoundry Interval { get; set; }
+        bool StopAtDurationEnd { get; set; }
+    }
 }
