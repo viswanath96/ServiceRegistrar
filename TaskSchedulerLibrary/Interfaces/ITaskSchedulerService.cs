e6f2e5d33ac73fd4899d34738d13a179f6ac67a8
viswanath <links96.4u@outlook.com>
2025-04-20 17:07:57 +0530

    Method signatures for registering the task.

--- TaskSchedulerLibrary/Interfaces/ITaskSchedulerService.cs
+++ TaskSchedulerLibrary/Interfaces/ITaskSchedulerService.cs
@@ -1,12 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using ServiceRegistrar.Models;
 
 namespace TaskSchedulerLibrary.Interfaces
 {
     public interface ITaskSchedulerService
     {
+        public void RegisterTask(AppRegistrationData appRegistrationData);
+        public void UnregisterTask(string taskName);
+        public void UpdateTask(AppRegistrationData appRegistrationData);
+        public void RunTask(string taskName);
+        public void StopTask(string taskName);
+        public void GetTaskStatus(string taskName);
+
     }
 }
