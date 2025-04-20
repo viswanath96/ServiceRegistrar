bd6b0e4daa1d2ffd6cb99070c53a763327d4120c
viswanath <links96.4u@outlook.com>
2025-04-20 17:20:02 +0530

    Added Task Scheduler service.

---
+++ TaskSchedulerLibrary/Services/TaskSchedulerService.cs
@@ -0,0 +1,43 @@
+﻿using ServiceRegistrar.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskSchedulerLibrary.Interfaces;
+
+namespace TaskSchedulerLibrary.Services
+{
+    public class TaskSchedulerService : ITaskSchedulerService
+    {
+        public void GetTaskStatus(string taskName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RegisterTask(AppRegistrationData appRegistrationData)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RunTask(string taskName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void StopTask(string taskName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UnregisterTask(string taskName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateTask(AppRegistrationData appRegistrationData)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
